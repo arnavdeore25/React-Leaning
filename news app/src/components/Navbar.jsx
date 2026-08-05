2dde61085ec3e70764244c27110d55eb0a3c6665
Arnav Deore <156898120+arnavdeore25@users.noreply.github.com>
2026-08-05 17:43:40 +0530

    Create Navbar.jsx

---
+++ news app/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white p-4 shadow-md">
+        <div className="max-w-7xl mx-auto flex justify-between items-center">
+            <h1 className="text-2xl font-bold">
+                News HUB
+            </h1>
+            <p className="text-sm font-normal">Stay Udated with what's going on</p>
+          
+        </div>
+
+</nav>
+
+  )
+}
+
+export default Navbar
